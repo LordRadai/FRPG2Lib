279113d03bea353ad084c488118e930ab1ca104d
Radai <lordradai@null.net>
2025-05-24 16:30:34 +0200

    Update DLVector.h

--- FRPG2Lib/FRPG2Lib/dantelion2/Core/Util/DLVector.h
+++ FRPG2Lib/FRPG2Lib/dantelion2/Core/Util/DLVector.h
@@ -7,26 +7,43 @@ namespace DLUT
 	template<typename T>
 	class DLVector : public std::vector<T>
 	{
+#ifndef _DEBUG
 		DLKR::DLAllocator* m_pAllocator;
+#endif
 
 	public:
-		DLVector() : std::vector<T>(), m_pAllocator(nullptr) {}
+		DLVector() : std::vector<T>() {}
 
-		DLVector(DLKR::DLAllocator* pAllocator) : std::vector<T>(), m_pAllocator(pAllocator) {}
-
-		DLVector(size_t size, DLKR::DLAllocator* pAllocator) : std::vector<T>(size), m_pAllocator(pAllocator)
+		DLVector(DLKR::DLAllocator* pAllocator) : std::vector<T>() 
 		{
+#ifndef _DEBUG
+			m_pAllocator = pAllocator;
+#endif
+		}
+
+		DLVector(size_t size, DLKR::DLAllocator* pAllocator) : std::vector<T>(size)
+		{
+#ifndef _DEBUG
+			m_pAllocator = pAllocator;
+#endif
 			this->reserve(size);
 		}
 
-		DLVector(const DLVector& other) : std::vector<T>(other), m_pAllocator(other.m_pAllocator) {}
+		DLVector(const DLVector& other) : std::vector<T>(other) 
+		{
+#ifndef _DEBUG
+			m_pAllocator = other.m_pAllocator;
+#endif
+		}
 
 		DLVector& operator=(const DLVector& other)
 		{
 			if (this != &other)
 			{
 				std::vector<T>::operator=(other);
+#ifndef _DEBUG
 				m_pAllocator = other.m_pAllocator;
+#endif
 			}
 			return *this;
 		}
